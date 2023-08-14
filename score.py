a63be1170c6257eef42b05045f2c9998f9d03c7d
KexySerJ90 <exserj9011@gmail.com>
2023-08-14 20:16:33 +0300

    Complete game

--- score.py
+++ score.py
@@ -3,8 +3,9 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        with open ('data.txt') as f:
+            self.high_score=int(f.read())
         self.point=0
-        self.high_score=0
         self.color('white')
         self.penup()
         self.goto(0, 270)
@@ -19,8 +20,10 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.point>self.high_score:
             self.high_score=self.point
-        self.point=0
-        self.update_score()
+            with open('data.txt', 'w') as f:
+                f.write(f'{self.high_score}')
+            self.point=0
+            self.update_score()
 
     def win(self):
         self.point+=1
